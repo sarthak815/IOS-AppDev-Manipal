f311d0ac071a5e2c77a19b8497243e11fcb38361
Sarthak Shastri <sarthak.shastri@learner.manipal.edu>
2022-09-23 15:44:14 +0530

    Class&Inheritance

--- 2 - Introduction to UIKit/4 - Classes and Inheritance/Lab - Classes.playground/Pages/4. Exercise - Class Memberwise Initializers and References.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/4 - Classes and Inheritance/Lab - Classes.playground/Pages/4. Exercise - Class Memberwise Initializers and References.xcplaygroundpage/Contents.swift
@@ -7,6 +7,13 @@ class Spaceship {
     let name: String
     var health: Int
     var position: Int
+    
+    init(name: String, health: Int, position: Int) {
+            self.name = name
+            self.health = health
+            self.position = position
+        }
+
 
     func moveLeft() {
         position -= 1
@@ -27,7 +34,13 @@ class Spaceship {
 class Fighter: Spaceship {
     let weapon: String
     var remainingFirePower: Int
-
+    
+    init(name: String, health: Int, position: Int, weapon: String, remainingFirePower: Int){
+        self.weapon = weapon
+        self.remainingFirePower = remainingFirePower
+        
+        super.init(name: name, health: health, position: position)
+    }
     func fire() {
         if remainingFirePower > 0 {
             remainingFirePower -= 1
@@ -39,7 +52,13 @@ class Fighter: Spaceship {
 
 class ShieldedShip: Fighter {
     var shieldStrength: Int
-
+    
+    init(name: String, health: Int, position: Int, weapon: String, remainingFirePower: Int, shieldStrength: Int) {
+           self.shieldStrength = shieldStrength
+           
+           super.init(name: name, health: health, position: position, weapon: weapon, remainingFirePower: remainingFirePower)
+       }
+       
     override func wasHit() {
         if shieldStrength > 0 {
             shieldStrength -= 5
@@ -53,23 +72,30 @@ class ShieldedShip: Fighter {
 
  Then create an instance of `Spaceship` below called `falcon`. Use the memberwise initializer you just created. The ship's name should be "Falcon."
  */
-
-
+let falcon = Spaceship(name: "Falcon", health: 100, position: 0)
 /*:
  Writing initializers for subclasses can get tricky. Your initializer needs to not only set the properties declared on the subclass, but also set all of the uninitialized properties on classes that it inherits from. Go to the declaration of `Fighter` and write an initializer that takes an argument for each property on `Fighter` and for each property on `Spaceship`. Set the properties accordingly. (Hint: you can call through to a superclass's initializer with `super.init` *after* you initialize all of the properties on the subclass).
 
  Then create an instance of `Fighter` below called `destroyer`. Use the memberwise initializer you just created. The ship's name should be "Destroyer."
  */
-
+let destroyer =  Fighter(name: "Destroyer", health: 150, position: 0, weapon: "Laser", remainingFirePower: 10)
 
 /*:
  Now go add an initializer to `ShieldedShip` that takes an argument for each property on `ShieldedShip`, `Fighter`, and `Spaceship`, and sets the properties accordingly. Remember that you can call through to the initializer on `Fighter` using `super.init`.
 
  Then create an instance of `ShieldedShip` below called `defender`. Use the memberwise initializer you just created. The ship's name should be "Defender."
  */
-
+let defender = ShieldedShip(name: "Defender", health: 200, position: 2, weapon: "Guns", remainingFirePower: 50, shieldStrength: 150)
 
 //:  Create a new constant named `sameShip` and set it equal to `falcon`. Print out the position of `sameShip` and `falcon`, then call `moveLeft()` on `sameShip` and print out the position of `sameShip` and `falcon` again. Did both positions change? Why? If both were structs instead of classes, would it be the same? Why or why not? Provide your answer in a comment or print statement below.
+let sameShip = falcon
+print("sameShip position:", sameShip.position)
+print("falcon position:", falcon.position)
+sameShip.moveLeft()
+print("sameShip position:", sameShip.position)
+print("falcon position:", falcon.position)
+print("The sameShip constant essentially stroes the memory location of the falcon class, so any changes made to sameShip will be reflected in falcon also")
+print("If both were structs this would not be the case as structs are of value type so the data in them are copied rather than being referenced")
 
 
 /*:
