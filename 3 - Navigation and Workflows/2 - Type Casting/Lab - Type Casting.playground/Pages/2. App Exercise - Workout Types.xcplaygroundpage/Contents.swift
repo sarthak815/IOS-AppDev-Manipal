17d89754956799ad41460ebf7a3381feb35eed26
Sarthak Shastri <55555570+sarthak815@users.noreply.github.com>
2022-10-04 16:09:52 +0530

    Optional, Enum ,Scope, TypeCast

--- 3 - Navigation and Workflows/2 - Type Casting/Lab - Type Casting.playground/Pages/2. App Exercise - Workout Types.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/2 - Type Casting/Lab - Type Casting.playground/Pages/2. App Exercise - Workout Types.xcplaygroundpage/Contents.swift
@@ -43,10 +43,24 @@ var workouts: [Workout] = [
     Run(cadence: 90, time: 358.9, distance: 1600)
 ]
 //:  Write simple functions called `describeRun(runningWorkout:)` and `describeSwim(swimmingWorkout:)` that take a `Run` object and a `Swim` object, respectively. Neither should return values. Each function should print a description of the workout, including the run's cadence or the swim's stroke. Time is represented in seconds, distance is represented in meters, and cadence is represented in steps per minute.
- 
+ func describeRun(runningWorkout: Run) {
+    print("Running workout: distance \(runningWorkout.distance) m, time \(runningWorkout.time) sec, cadence \(runningWorkout.cadence) steps per minnute.")
+    
+}
+
+func describeSwim(swimmingWorkout: Swim) {
+    print("Swimming workout: distance \(swimmingWorkout.distance) m, time \(swimmingWorkout.time) sec, cadence \(swimmingWorkout.stroke).")
+}
 
 //:  Now loop through each workout in `workouts` and, using type casting, call either `describeRun(runningWorkout:)` or `describeSwim(swimmingWorkout:)` on each. Observe what is printed to the console.
- 
+ for workout in workouts {
+    if let run = workout as? Run {
+        describeRun(runningWorkout: run)
+    } else if let swim = workout as? Swim {
+        describeSwim(swimmingWorkout: swim)
+        
+    }
+}
 
 /*:
  _Copyright © 2021 Apple Inc._
--- 3 - Navigation and Workflows/4 - Scope/Lab - Scope.playground/Pages/1. Exercise - Scope.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/4 - Scope/Lab - Scope.playground/Pages/1. Exercise - Scope.xcplaygroundpage/Contents.swift
@@ -8,6 +8,7 @@ for _ in 0..<10 {
     print("The value of foo is \(foo)")
 }
 //print("The value of foo is \(foo)")
+//The 10th line generates a compiler error as foo is declared inside the for loop and is out of scope for the print statement
 
 
 //:  Using a comment or print statement, describe why both print statements below compile when similar-looking code did not compile above. In what scope is `x` defined, and in what scope is it modified? In contrast, in what scope is `foo` defined and used?
@@ -17,16 +18,31 @@ for _ in 0..<10 {
     print("The value of x is \(x)")
 }
 print("The final value of x is \(x)")
-
+// it uses x from outside scope, the same variable then modified inside the function, and foo was defined in outer scope
 
 //:  In the body of the function `greeting` below, use variable shadowing when unwrapping `greeting`. If `greeting` is successfully unwrapped, print a statement that uses the given greeting to greet the given name (i.e. if `greeting` successfully unwraps to have the value "Hi there" and `name` is `Sara`, print "Hi there, Sara."). Otherwise, use "Hello" to print a statement greeting the given name. Call the function twice, once passing in a value for greeting, and once passing in `nil`.
 func greeting(greeting: String?, name: String) {
-
+    guard let greeting = greeting else {
+        print("Hello \(name)")
+        return
+    }
+    print("\(greeting) \(name)")
 }
-
+greeting(greeting: "Hello there", name: "Sara")
+greeting(greeting: nil, name: "Sara")
 
 //:  Create a class called `Car`. It should have properties for `make`, `model`, and `year` that are of type `String`, `String`, and `Int`, respectively. Since this is a class, you'll need to write your own memberwise initializer. Use shadowing when naming parameters in your initializer.
-
+class Car {
+    var make: String
+    var model: String
+    var year: Int
+    
+    init(make: String, model: String, year: Int) {
+        self.make = make
+        self.model = model
+        self.year = year
+    }
+}
 
 /*:
 page 1 of 2  |  [Next: App Exercise - Step Competition](@next)
