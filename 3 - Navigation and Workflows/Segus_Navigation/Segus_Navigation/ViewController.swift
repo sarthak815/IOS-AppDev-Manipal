f0c919ffb8117b7d191329ed1ab3eb9d5651e9b1
Sarthak Shastri <sarthak.shastri@learner.manipal.edu>
2022-10-07 18:24:19 +0530

    Segue

---
+++ 3 - Navigation and Workflows/Segus_Navigation/Segus_Navigation/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Segus_Navigation
+//
+//  Created by Sarthak Shastri on 07/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+        @IBOutlet weak var forgotUserNameButton: UIButton!
+        @IBOutlet weak var forgotPasswordButton: UIButton!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+        }
+
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            guard let sender = sender as? UIButton else { return }
+            
+            if sender == forgotUserNameButton {
+                segue.destination.navigationItem.title = "Forgot User Name"
+            } else if sender == forgotPasswordButton {
+                segue.destination.navigationItem.title = "Forgot Password"
+            } else {
+                segue.destination.navigationItem.title = userNameTextField.text
+            }
+        }
+        
+        @IBAction func forgotUserNameButtonTapped(_ sender: UIButton) {
+            performSegue(withIdentifier: "ForgottenUserNameOrPassword", sender: sender)
+        }
+        
+        @IBAction func forgotPasswordButtonTapped(_ sender: UIButton) {
+            performSegue(withIdentifier: "ForgottenUserNameOrPassword", sender: sender)
+        }
+
+}
+
