63da8d44b63d7172886468ffad9dcdb4414ceeef
Sarthak Shastri <sarthak.shastri@learner.manipal.edu>
2022-10-07 18:17:52 +0530

    Stack_Layout

---
+++ 2 - Introduction to UIKit/Stack/Stack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stack
+//
+//  Created by Sarthak Shastri on 07/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
